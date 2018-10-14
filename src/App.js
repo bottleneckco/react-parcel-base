2abd298d2976674ad5944273f9841682dc5a8f8c
Duncan Leo <duncanleo97@gmail.com>
2018-10-14 17:17:11 +0800

    update base files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import ShopList from './components/ShopList';
 import './stylesheets/styles.scss';
 
 const App = () => (
   <div>
-    <header>
-      <Header />
-    </header>
     <main>
-      <ShopList />
+      <span>Hi there!</span>
     </main>
   </div>
 );
